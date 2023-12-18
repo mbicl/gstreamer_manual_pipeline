6e7caf263f9a4672ec81d6c7cb68e11ccd20a072
mbicl <baxriddinovmaqsud@gmail.com>
2023-12-18 17:19:55 +0500

    added choosing pattern and speed features

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Manual pipeline in GStreamer
 This program opens a window and displays a test pattern, without audio.
+Program asks for input value for pattern type, you can enter integer value.
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <gst/gst.h>
+#include <stdio.h>
 
 int t_main(int argc, char *argv[]){
-    GstElement *pipeline, *source, *sink;
+    GstElement *pipeline, *source, *sink, *filter, *videoconvert;
     GstBus *bus;
     GstMessage *msg;
     GstStateChangeReturn ret;
@@ -9,24 +10,36 @@ int t_main(int argc, char *argv[]){
     gst_init(&argc,&argv);
 
     source = gst_element_factory_make("videotestsrc","source");
+    filter = gst_element_factory_make("vertigotv","filter");
+    videoconvert = gst_element_factory_make("videoconvert","videoconvert");
     sink = gst_element_factory_make("autovideosink","sink");
 
     pipeline = gst_pipeline_new("test-pipeline");
 
-    if (!pipeline || !source || !sink){
+    if (!pipeline || !source || !sink || !filter || !videoconvert){
         g_printerr("Not all elements could be created\n");
         return -1;
     }
 
-    gst_bin_add_many(GST_BIN(pipeline),source,sink,NULL);
+    gst_bin_add_many(GST_BIN(pipeline),source,filter,videoconvert,sink,NULL);
 
-    if (gst_element_link(source,sink)!=TRUE) {
+    if (gst_element_link(source,filter)!=TRUE || 
+        gst_element_link(filter,videoconvert)!=TRUE ||
+        gst_element_link(videoconvert,sink)!=TRUE
+    ) {
         g_printerr("Elements could not be linked.\n");
         gst_object_unref(pipeline);
         return -1;
     }
+    gint pt;
+    printf("Enter pattern type: ");
+    scanf("%d",&pt);
+    g_object_set(source,"pattern",pt,NULL);
 
-    g_object_set(source,"pattern",0,NULL);
+    gfloat speed;
+    printf("Enter speed of video (default: 0.02): ");
+    scanf("%f",&speed);
+    g_object_set(filter,"zoom-speed",speed,NULL);
 
     ret = gst_element_set_state(pipeline,GST_STATE_PLAYING);
     if (ret == GST_STATE_CHANGE_FAILURE){
